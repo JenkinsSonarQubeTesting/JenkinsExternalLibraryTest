0e23dcc8636bfd69e1529bd91d46393145099b3c
Carter Daly-MacPhail <carter.daly-macphail@verafin.com>
2018-06-21 14:55:54 -0230

    Jenkins shared library test

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -2,7 +2,7 @@
 
 import com.amazonaws.services.s3.*
 
-class Test{
+class Test implements Serializable{
   def String call(message){
     testText = message
     echo message
@@ -14,4 +14,4 @@ class Test{
     echo text
   }
 }
-
+return this
