968ac93710839ed983f3925336bf7ac3bacd8eb4
Carter Daly-MacPhail <carter.daly-macphail@verafin.com>
2018-06-21 13:27:39 -0230

    Jenkins shared library test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,8 +1,8 @@
 //import com.testingJavaImports.TestJava
+@Grab(group='com.amazonaws', module='aws-java-sdk-s3', version='1.11.349')
 import com.amazonaws.services.s3.*
 
 def String call(message){
-  @Grab(group='com.amazonaws', module='aws-java-sdk-s3', version='1.11.349')
   testText = message
   echo message
 }
