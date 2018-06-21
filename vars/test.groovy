272d310a2d8698e0443f0c52bca49431b039e2a5
Carter Daly-MacPhail <carter.daly-macphail@verafin.com>
2018-06-21 13:50:44 -0230

    Jenkins shared library test

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,10 +3,13 @@ import groovy.grape.Grape
 //@groovy.lang.GrabConfig(systemClassLoader = true)
 //@Grab(group='com.amazonaws', module='aws-java-sdk-s3', version='1.11.349')
 
-Grape.grab(group='com.amazonaws', module='aws-java-sdk-s3', version='1.11.349')
+//Grape.grab(group='com.amazonaws', module='aws-java-sdk-s3', version='1.11.349')
 import com.amazonaws.services.s3.*
 
 def String call(message){
+  ClassLoader cl = new groovy.lang.GroovyClassLoader()
+  Grape.grab(group='com.amazonaws', module='aws-java-sdk-s3', version='1.11.349', classLoader=cl)
+
   AmazonS3Client s3 = new AmazonS3Client()
   testText = message
   echo message
