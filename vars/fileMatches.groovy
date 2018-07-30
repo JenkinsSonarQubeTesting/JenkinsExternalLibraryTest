49f77efc0a50d21d7d1cca653b33ff661e0681f1
Carter Daly-MacPhail <cdalymac@gmail.com>
2018-07-30 10:55:09 -0230

    Update fileMatches.groovy

--- vars/fileMatches.groovy
+++ vars/fileMatches.groovy
@@ -1,23 +1,19 @@
 def call(body){
-  try{
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
 
-    List<String> files = config.fileNames.getValues()
-    def pattern = ~"Jenkins(.*)|(.*)test(.*)"
+  List<String> files = config.fileNames.getValues()
+  def pattern = ~"Jenkins(.*)|(.*)test(.*)"
 
-    List<String> nonMatches = files.flatten().findAll{ !pattern.matcher(it).find() }
-    if(nonMatches.isEmpty()){
-      return true 
-    }
-    else{
-      echo "${nonMatches.size()} file(s) don't match pattern:\n"
-      nonMatches.each{echo it.toString()}
-      return false
-    }
-  }catch(Exception e){
-   echo e.getMessage() 
+  List<String> nonMatches = files.flatten().findAll{ !pattern.matcher(it).find() }
+  if(nonMatches.isEmpty()){
+    return true 
+  }
+  else{
+    echo "${nonMatches.size()} file(s) don't match pattern:\n"
+    nonMatches.each{echo it}
+    return false
   }
 }
