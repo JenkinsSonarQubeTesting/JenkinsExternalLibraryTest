b2476a3018f05617c48996fb037953a11653a692
Carter Daly-MacPhail <cdalymac@gmail.com>
2018-07-30 10:08:00 -0230

    Update and rename test.groovy to fileMatches.groovy

---
+++ vars/fileMatches.groovy
@@ -0,0 +1,19 @@
+def call(body){
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
+  List<String> files = config.fileNames
+  
+  def pattern = ~"Jenkins(.*)"
+  
+  List<String> nonMatches = files.findAll{ !pattern.matcher(it).find() }
+  if(nonMatches.isEmpty()){
+    return true 
+  }
+  else{
+    echo "${nonMatches.size()} file(s) don't match pattern:\n"
+    nonMatches.each{echo it}
+    return false
+  }
